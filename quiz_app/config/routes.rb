3881be9c82652206bbc992a6226f4310cf02778f
Michael Dennis <michaelsdennis4@gmail.com>
2015-06-23 12:48:23 -0400

    created sessions controller

--- quiz_app/config/routes.rb
+++ quiz_app/config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+
+  root 'sessions#new'
+
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  resources :users
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
