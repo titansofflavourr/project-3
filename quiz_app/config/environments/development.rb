8e4cf302d8141685e519a3781c63b6ba1289d434
Junho Rhee <juno0628@gmail.com>
2015-06-23 19:15:19 -0400

    mail

--- quiz_app/config/environments/development.rb
+++ quiz_app/config/environments/development.rb
@@ -40,15 +40,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.active_record.migration_error = :page_load
-  config.action_mailer.perform_deliveries  = false 
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i -t'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@example.com'}
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "example.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]  
-  }
 end
