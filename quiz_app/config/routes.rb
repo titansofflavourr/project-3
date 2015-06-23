17ec3931628abfa7aabdb0e2109962696ea07b8d
Michael Dennis <michaelsdennis4@gmail.com>
2015-06-23 13:17:47 -0400

    created new migration - add comment to responses

--- quiz_app/config/routes.rb
+++ quiz_app/config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
 
   resources :users
- 
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
