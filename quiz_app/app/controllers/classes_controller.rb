151f42e47c3ee95bec8e5337d75b8a5f1a993bba
Michael Dennis <michaelsdennis4@gmail.com>
2015-06-23 16:27:30 -0400

    working on controllers

--- quiz_app/app/controllers/classes_controller.rb
+++ quiz_app/app/controllers/classes_controller.rb
@@ -1,19 +1,35 @@
 class ClassesController < ApplicationController
+
   def index
+    @classes = Class.all
   end
 
   def create
+    xclass = Class.new
+    xclass.update(class_params)
   end
 
   def new
+    @class = Class.new
   end
 
   def show
+    @class = Class.find(params[:id])
   end
 
   def update
+    xclass = Class.find(params[:id])
+    xclass.update(class_params)
   end
 
   def edit
+    @class = Class.find(params[:id])
   end
+
+  private
+
+  def class_params
+    params.require(:class).permit(:f_name, :l_name, :email, :password, :password_confirmation)
+  end
+
 end
--- quiz_app/app/controllers/users_controller.rb
+++ quiz_app/app/controllers/users_controller.rb
@@ -7,9 +7,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new
+    if user.update(user_params)
+      session[:user_id] = user.id
+      session[:is_instructor] = user.is_instructor
+      redirect_to user_path(user)
+    else
+      # rerender the form
+      redirect_to '/users/new'
+    end
   end
 
   def new
+    @user = User.new
   end
 
   def show
@@ -17,9 +27,20 @@ class UsersController < ApplicationController
   end
 
   def update
+    user = User.find(session[:user_id])
+    user.update(user_params)
+    redirect_to user_path(user) 
   end
 
   def edit
+    # can instructors edit students' profiles?
+    @user = User.find(session[:user_id]) 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:f_name, :l_name, :email, :password, :password_confirmation)
   end
 
 end
