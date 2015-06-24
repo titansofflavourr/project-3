cb1d4c9c603820fa06aab3f346388b6f3a72fc64
Gabriella Lubliner <gabriella.lubliner@gmail.com>
2015-06-24 16:49:16 -0400

    finished basic dashboard styling

--- quiz_app/app/controllers/dashboards_controller.rb
+++ quiz_app/app/controllers/dashboards_controller.rb
@@ -1,6 +1,10 @@
 class DashboardsController < ApplicationController
 
 	def index
+    @users = User.all
+    @cohorts = Cohort.all
+    @quizzes = Quiz.all
+    @user = User.find(session[:user_id])
 		render :dashboard
 	end
 
