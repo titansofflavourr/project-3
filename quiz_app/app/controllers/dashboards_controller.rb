ba57f89b30e5bd39f1253bc7a3c55f448e5bf240
Michael Dennis <michaelsdennis4@gmail.com>
2015-06-24 16:53:00 -0400

    configure root

--- quiz_app/app/controllers/dashboards_controller.rb
+++ quiz_app/app/controllers/dashboards_controller.rb
@@ -1,7 +1,13 @@
 class DashboardsController < ApplicationController
 
+	before_action :authorize
+
 	def index
-		render :dashboard
+		if (session[:user_id])
+			render :dashboard
+		else
+			redirect_to '/login'
+		end
 	end
 
 end
