96ef9c915c37764b1003e4841d89582cf8b05806
Michael Dennis <michaelsdennis4@gmail.com>
2015-06-26 11:35:25 -0400

    updates

--- quiz_app/config/routes.rb
+++ quiz_app/config/routes.rb
@@ -33,6 +33,8 @@ Rails.application.routes.draw do
 
   # resources :cohorts, :except =>[:destroy]
 
+  get 'quiz', to: 'quizzes#take'
+
   get 'quizzes/report', to: 'quizzes#report'
 
   resources :quizzes, :except =>[:destroy]
