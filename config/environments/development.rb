d77b5ca3db6e86b3b27ad8b350705d9e37c0b921
Michael Dennis <michaelsdennis4@gmail.com>
2015-06-26 17:43:18 -0400

    precompile assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,6 +25,8 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.assets.prefix = "/dev-assets"
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -50,6 +52,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'no-reply-email-from-quizhulu@example.com'}
 
-  config.assets.prefix = "/dev-assets"
+  
 
 end
