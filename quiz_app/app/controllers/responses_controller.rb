3fa84f054b809b699e420f936f84d53c33cd6541
Junho Rhee <juno0628@gmail.com>
2015-06-26 03:43:53 -0400

    student quiz multiple choice added

--- quiz_app/app/controllers/responses_controller.rb
+++ quiz_app/app/controllers/responses_controller.rb
@@ -5,14 +5,11 @@ class ResponsesController < ApplicationController
   end
 
   def create
-<<<<<<< HEAD
-    @response = Response.create(user_id: session[:user_id], question_id: params[:question_id],answer: params[:answer])
-=======
+    binding.pry
     # creating response using session and ajax params
     @response = Response.create(user_id: session[:user_id], question_id: params[:question_id],answer: params[:answer])
     
     # send response back to browser
->>>>>>> 03a482b51a16cdcf497d14ad4791f9bee63142e9
     render json: @response
 
     
@@ -29,6 +26,7 @@ class ResponsesController < ApplicationController
   end
 
   def update
+    binding.pry
     student_response = Response.find(params[:id])
     @response = student_response.update(answer: params[:answer])
     render json: @response 
