5a6a63664f08982d14d79ba6cd663027811cdb9d
Michael Dennis <michaelsdennis4@gmail.com>
2015-06-28 22:31:33 -0400

    wtf

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -19,35 +19,6 @@ class QuestionsController < ApplicationController
     end
   end
 
-  def add #ajax call
-    if (params[:is_multiple_choice] == "true")
-      answer = params[:mc_answer_key]
-    else
-      answer = params[:sa_answer_key]
-    end
-    question = Question.create({prompt: params[:prompt], answer_key: answer, is_multiple_choice: params[:is_multiple_choice], max_points: params[:max_points], quiz_id: params[:quiz_id]}); 
-    if (params[:is_multiple_choice] == "true")
-      if (params[:choice1] != "")
-        key = 'a'
-        question.choices.create({question_id: question.id, option: params[:choice1], key: key})
-      end
-      if (params[:choice2] != "")
-        key = 'b'
-        question.choices.create({question_id: question.id, option: params[:choice2], key: key})
-      end
-      if (params[:choice3] != "")
-        key = 'c'
-        question.choices.create({question_id: question.id, option: params[:choice3], key: key})
-      end
-      if (params[:choice4] != "")
-        key = 'd'
-        question.choices.create({question_id: question.id, option: params[:choice4], key: key})
-      end
-      if (params[:choice5] != "")
-        key = 'e'
-        question.choices.create({question_id: question.id, option: params[:choice5], key: key})
-      end
-    end
     # choices.each do |option|
     #   if (option != "")
     #     key = (i+97).chr
@@ -76,6 +47,36 @@ class QuestionsController < ApplicationController
   def edit
     @question = Question.find(params[:id])
   end
+
+   def add #ajax call
+    if (params[:is_multiple_choice] == "true")
+      answer = params[:mc_answer_key]
+    else
+      answer = params[:sa_answer_key]
+    end
+    question = Question.create({prompt: params[:prompt], answer_key: answer, is_multiple_choice: params[:is_multiple_choice], max_points: params[:max_points], quiz_id: params[:quiz_id]}); 
+    if (params[:is_multiple_choice] == "true")
+      if (params[:choice1] != "")
+        key = 'a'
+        question.choices.create({question_id: question.id, option: params[:choice1], key: key})
+      end
+      if (params[:choice2] != "")
+        key = 'b'
+        question.choices.create({question_id: question.id, option: params[:choice2], key: key})
+      end
+      if (params[:choice3] != "")
+        key = 'c'
+        question.choices.create({question_id: question.id, option: params[:choice3], key: key})
+      end
+      if (params[:choice4] != "")
+        key = 'd'
+        question.choices.create({question_id: question.id, option: params[:choice4], key: key})
+      end
+      if (params[:choice5] != "")
+        key = 'e'
+        question.choices.create({question_id: question.id, option: params[:choice5], key: key})
+      end
+    end
 
   private
 
