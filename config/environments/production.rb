7fd3afb3059bf2c7682f55b87fbe47a4d1af3810
Junho Rhee <juno0628@gmail.com>
2015-06-29 09:39:05 -0400

    mailer change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.aciton_mailer.default_options = {from: 'no-reply-email-from-quizzulhu@example.com'}
+  config.action_mailer.default_options = {from: 'no-reply-email-from-quizzulhu@example.com'}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
